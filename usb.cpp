af177dbf3e30d628cb06933cb95cf17391352311
kabu3125 <kavyanautiyal3125@gmail.com>
2024-04-03 12:21:50 -0700

    Create usb.cpp

---
+++ usb.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"enter the number"<<endl;
+    cin>>number;
+    for (int i = 0; i <= 10; i++)
+    {
+       cout<<i*number<<endl;
+    }
+    return 0;
+}
