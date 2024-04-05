3b095b10a76fdc182083dbd18830942747bf39e6
kabu3125 <kavyanautiyal3125@gmail.com>
2024-04-04 18:38:53 -0700

    Create break.cpp

---
+++ break.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i <=40; i++)
+    {
+        cout<<i<<endl;
+        if(i==7){
+            break;
+        }
+    }
+    
+    return 0;
+}
