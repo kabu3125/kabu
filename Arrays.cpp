004c0df0e7242fc69e72ce37a600d2a3947315f3
kabu3125 <kavyanautiyal3125@gmail.com>
2024-04-06 17:34:14 -0700

    Create Arrays.cpp

---
+++ Arrays.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[]= {23,45,67,89,90};
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<marks[i]<<endl;
+    }
+    
+    return 0;
+}
